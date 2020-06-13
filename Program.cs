87b2f7f7d4c5cf54d0b11e1d996235b858b983fb
Akbar <akbar.26@students.amikom.ac.id>
2020-06-13 17:49:51 +0800

    inisiasi project

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+//Abstrac Class
+public abstract class G
+{
+
+     
+    public abstract void gfg1();
+}
+
+ 
+public class G1 : G
+{
+
+
+    public override void gfg1()
+    {
+        Console.WriteLine("Abstrac Class");
+        Console.WriteLine("Nama     :   Akbar");
+        Console.WriteLine("NIM      :   19.11.2778");
+        Console.WriteLine("Kelas    :   19IF03");
+    }
+
+
+}
+//Interface
+interface interface1
+{
+
+  
+    void show();
+}
+
+class MyClass : interface1
+{
+
+  
+    public void show()
+    {
+        Console.WriteLine("Interface");
+        Console.WriteLine("Universitas Amikom Yogyakarta");
+    }
+    public class main_method
+    {
+         
+        public static void Main(string[] args)
+        {
+
+            G obj;
+
+            obj = new G1();
+
+
+            obj.gfg1();
+            Console.WriteLine();
+
+            MyClass obj1 = new MyClass();
+
+ 
+            obj1.show();
+
+            
+        }
+    }
+}
+
+
